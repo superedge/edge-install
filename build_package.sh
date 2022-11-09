7537a67bbef902f7df41e49370982b96371686c6
dodiadodia <dodia@163.com>
2022-11-09 11:25:53 +0800

    add build package shell

---
+++ build_package.sh
@@ -0,0 +1,18 @@
+#!/usr/bin
+
+DOCKER_VERSION="docker-19.03-linux-arm64.tgz"
+CONTAINERD_VERSION="containerd-1.3.4-linux-arm64.tgz"
+KUBE_VERSION="kube-linux-arm64-v1.22.6.tar.gz"
+
+WORKDIR=`pwd`
+cd edge-install/container/
+tar zcvf $DOCKER_VERSION ./docker/
+tar zcvf $CONTAINERD_VERSION ./containerd/
+rm -rf ./docker
+rm -rf ./containerd
+
+cd $WORKDIR
+rm -rf edge-install/.git
+tar zcvf $KUBE_VERSION ./edge-install
+
+
