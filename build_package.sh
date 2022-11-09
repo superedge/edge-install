64285695ab8cc72f8df205b10db9ed20f269e307
dodiadodia <dodia@163.com>
2022-11-09 15:55:41 +0800

    modify build_package version

--- build_package.sh
+++ build_package.sh
@@ -2,7 +2,7 @@
 
 DOCKER_VERSION="docker-19.03-linux-amd64.tgz"
 CONTAINERD_VERSION="containerd-1.4.3-linux-amd64.tgz"
-KUBE_VERSION="kube-linux-arm64-v1.20.6.tar.gz"
+KUBE_VERSION="kube-linux-amd64-v1.20.6.tar.gz"
 
 WORKDIR=`pwd`
 cd edge-install/container/
